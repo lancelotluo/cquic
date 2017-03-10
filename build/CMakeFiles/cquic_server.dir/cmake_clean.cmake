02c429e1dd80e6103f624bcb9aef67c510db3cfc
luocn99 <luocn99@163.com>
2017-03-10 09:57:01 +0800

    add goquic, proto-quic

---
+++ build/CMakeFiles/cquic_server.dir/cmake_clean.cmake
@@ -0,0 +1,10 @@
+file(REMOVE_RECURSE
+  "CMakeFiles/cquic_server.dir/src/main.cc.o"
+  "cquic_server.pdb"
+  "cquic_server"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang CXX)
+  include(CMakeFiles/cquic_server.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
